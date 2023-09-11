1b825cd9c2c61a72de259445382c02d77d427a9d
Chaoyang Mei <136108307+Johnmcy@users.noreply.github.com>
2023-09-10 18:02:12 -0600

    Initialize

---
+++ RK_firstorder.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from numpy import *
+
+# dy/dx = -sin(x)
+def f(x, y):
+    return -sin(x)
+
+# initial condition when x = 0, y = 1
+x = [0]
+y = [1]
+
+# h:step N:total number
+h = 0.01
+N = 1000
+
+
+
+
+
+
