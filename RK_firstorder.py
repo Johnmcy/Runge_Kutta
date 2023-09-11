0bd4b965d6ca5167c2e2c65d0a50d55ffc9cca58
Chaoyang Mei <136108307+Johnmcy@users.noreply.github.com>
2023-09-10 18:03:33 -0600

    Achieve RK method and plot

--- RK_firstorder.py
+++ RK_firstorder.py
@@ -13,7 +13,20 @@ y = [1]
 h = 0.01
 N = 1000
 
+for i in range(N):
+    K1 = f(x[-1], y[-1])
+    K2 = f(x[-1] + 0.5 * h, y[-1] + 0.5 * h * K1)
+    K3 = f(x[-1] + 0.5 * h, y[-1] + 0.5 * h * K2)
+    K4 = f(x[-1] + h, y[-1] + h * K3)
+    x.append(x[-1] + h)
+    y.append(y[-1] + h/6 * (K1 + 2 * K2 + 2 * K3 + K4))
 
+plt.plot(x, y)
+plt.title('The original function of y = -sin(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+# plt.legend()
+plt.show()
 
 
 
